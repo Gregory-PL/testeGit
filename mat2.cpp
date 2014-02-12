53490544d72740beb6dc9ce1b16187c1ef1a9d5c
grelap <glpereira@ymail.com>
2014-02-12 21:23:27 -0200

    problema resolvido v1.1. Inserida a funcao correta

--- mat2.cpp
+++ mat2.cpp
@@ -4,3 +4,10 @@
 float dist(float p1, float p2){
     return sqt(p1 - p2);
 }
+
+int somaVet(int *vet, int tam){
+    int soma = 0;
+    for (int i = 0; i < tam; i++)
+        soma += vet[i];
+    return  soma;
+}
